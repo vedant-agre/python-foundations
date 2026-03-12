c621442d1441535b7362b50810c52dc498f939eb
vedant-agre <agrevedant316@gmail.com>
2026-03-12 19:07:20 +0530

    .

--- practice-set-6/04.py
+++ practice-set-6/04.py
@@ -1,2 +1,10 @@
-username = input("ENter username: ")
-print(len(username))
+'''12/06/26
+Write a program to find whether a given username contains less than 10 
+characters or not'''
+
+username = input("Enter username: ")
+
+if(len(username)<10):
+    print("Your username contains less than 10 characters")
+else:
+    print("Your username contains more than or equal to 10 characters")
